a13a3b95fa43e486b5b50542199adbb92e026249
anamikagawandi <anamika.gawandi@gmail.com>
2022-03-27 21:34:34 +0530

    Basic websocket connection setup between client and server

--- client/src/components/ChatWidget/Footer/Footer.js
+++ client/src/components/ChatWidget/Footer/Footer.js
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
     Input,
     Button
 } from 'antd';
 import { SendOutlined } from '@ant-design/icons';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+const client = new W3CWebSocket('ws://127.0.0.1:8000', 'echo-protocol');
 
 const Footer = (props) => {
     const { styles, setMessage } = props;
@@ -19,7 +22,23 @@ const Footer = (props) => {
             });
             setTextInput("");
         }
+        client.send(textInput);
     }
+
+    useEffect(() => {
+        client.onopen = () => {
+            console.log('WebSocket Client Connected');
+          };
+          client.onmessage = (message) => {
+            const reply = JSON.parse(message.data).message;
+            console.log(reply);
+            setMessage({
+                type: "bot",
+                message: reply,
+                timeStamp: new Date().getTime()
+            });
+          };
+    }, []);
 
     return (<div className={styles.input}>
         <Input.Group compact>
